570a62fd615f690f6548832b7531e0698b44d4b4
Craig Dennis <cdennis@twilio.com>
2019-02-11 21:04:32 -0800

    Changes to use templating language

--- src/main/java/io/github/craigsdennis/SmsController.java
+++ src/main/java/io/github/craigsdennis/SmsController.java
@@ -3,23 +3,20 @@ package io.github.craigsdennis;
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
-@RestController
+@Controller
 public class SmsController {
 
-    @PostMapping(value = "/sms", produces = "application/xml")
-    public String sms(WebRequest request) {
+    @PostMapping("/sms")
+    public String sms(WebRequest request, Model model) {
         String country = request.getParameter("FromCountry");
-        String txt = String.format("Hi! It looks like your phone number was born in %s", country);
-        MessagingResponse twiml = new MessagingResponse.Builder()
-                .message(new Message.Builder()
-                        .body(new Body.Builder(txt).build())
-                        .build())
-                .build();
-        return twiml.toXml();
+        model.addAttribute("txt", String.format("Hi! It looks like your phone number was born in %s", country));
+        return "sms";
 
     }
 }
